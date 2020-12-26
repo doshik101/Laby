4dec7b0346f5c1a476ce7761f51a6b01238aeec4
doshik101 <71586911+doshik101@users.noreply.github.com>
2020-12-26 10:25:16 +0300

    Create 3.py

---
+++ laba3/3.py
@@ -0,0 +1,5 @@
+friends = ['Danir', 'Raushan', 'Dima', 'Oleg']
+pop_names = ['Oleg', 'Ivan', 'Dima', 'Tema', 'Alegsey', 'Alexander', 'Matvey', 'Anasteysha', 'Karina', 'Anna']
+for i in range (len(friends)):
+    if friends[i] in pop_names:
+        print(friends[i])
