e696e3456b4c8ba245383cdd0ee5fa85dc474190
doshik101 <71586911+doshik101@users.noreply.github.com>
2020-12-26 10:21:58 +0300

    Create 2.py

---
+++ laba3/2.py
@@ -0,0 +1,9 @@
+i = ('Raushan', 'Gabdrafikov', 75)
+people = []
+people.append(i)
+friend1 = ('Keanu', 'Reeves', 56)
+people.append(friend1)
+friend2 = ('Obi-Wan', 'Kenobi', 19)
+people.append(friend2)
+people.sort()
+print(people)
